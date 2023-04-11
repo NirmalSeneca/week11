cda8f6cb5548009e75ca5b9bfb03bcca38b4fe6c
NirmalSeneca <106837327+NirmalSeneca@users.noreply.github.com>
2023-04-10 22:44:12 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "aws_security_group" "web-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.20.1.2/24"]
   }
 }
 
